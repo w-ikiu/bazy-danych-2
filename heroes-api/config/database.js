cca7066d08c7329f3dfe73a1758b7b09d95c150b
w-ikiu <chelminska.wiktoria@wp.pl>
2026-03-25 12:14:46 +0100

    removed sequelize and initialized prisma, created the schema

--- heroes-api/config/database.js
+++
@@ -1,9 +0,0 @@
-// plik konfigurujacy jedno, glowne polaczenie z baza danych (singleton)
-const { Sequelize } = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-// pobieramy parametry z naszego pliku config.js
-const config = require('./config.js')[env];
-
-const sequelize = new Sequelize(config.url, config);
-
-module.exports = sequelize;
--- heroes-api/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../config/database');
-const Hero = require('./hero');
-const Incident = require('./incident');
-
-// definicja asocjacji 1:n
-Hero.hasMany(Incident, { foreignKey: 'hero_id', as: 'incidents' });
-Incident.belongsTo(Hero, { foreignKey: 'hero_id', as: 'hero' });
-
-module.exports = { sequelize, Hero, Incident };
